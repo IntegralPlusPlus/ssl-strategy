bf54c0e95d54f1299229eeccbcbcf3f0bb0bb1f1
BAV <integralplusc@gmail.com>
2023-11-28 07:25:10 -0800

    Create shot point choosing

--- bridge/processors/python_controller.py
+++ bridge/processors/python_controller.py
@@ -18,9 +18,15 @@ import bridge.processors.auxiliary as auxiliary
 # TODO: Refactor this class and corresponding matlab scripts
 @attr.s(auto_attribs=True)
 class MatlabController(BaseProcessor):
+    xR = 4500
+    yR = 0
 
     processing_pause: typing.Optional[float] = 0.001
     max_commands_to_persist: int = 20
+    state = 0
+
+    wasBallKicked = False
+    firstBallX = -6000
 
     vision_reader: DataReader = attr.ib(init=False, default=DataReader(config.VISION_DETECTIONS_TOPIC))
     referee_reader: DataReader = attr.ib(init=False, default=DataReader(config.REFEREE_COMMANDS_TOPIC))
@@ -29,6 +35,7 @@ class MatlabController(BaseProcessor):
     cur_time = time.time()
     dt = 0
     ball = auxiliary.Point(0, 0)
+    count = 0
 
     b_team = team.Team(const.GK)
     y_team = team.Team(const.ENEMY_GK)
@@ -45,6 +52,9 @@ class MatlabController(BaseProcessor):
         if referee_commands:
             return referee_commands[-1]
         return RefereeCommand(0, 0, False)
+
+    def returnFirstElement(a):
+        return a[0]
 
     async def process(self) -> None:
         balls = np.zeros(const.BALL_PACKET_SIZE * const.MAX_BALLS_IN_FIELD)
@@ -77,9 +87,180 @@ class MatlabController(BaseProcessor):
             # referee_command = self.get_last_referee_command()
             rules = []
 
+            #self.b_team.robot(0).rotate_to_point(auxiliary.Point(self.xR, self.yR))
             # Strategy code start
+            if self.state == 0: self.b_team.robot(0).rotate_to_point(auxiliary.Point(self.ball.x, self.ball.y))
+            elif self.state == 1: self.b_team.robot(0).rotate_to_point(auxiliary.Point(self.xR, self.yR))
+            R = 200
+            r = 110
+            #xR = 4500
+            #yR = 0
+            x1 = self.b_team.robot(0).x
+            y1 = self.b_team.robot(0).y
+            x0 = self.ball.x
+            y0 = self.ball.y
+            goalUp = 500
+            goalDown = -500
+            n = 6
 
-            # Strategy code end
+            xAnti = []
+            yAnti = []
+            xAnti.append(4500)
+            yAnti.append(goalDown)
+
+            for i in range(n):
+                xAnti.append(self.y_team.robot(i).x)
+                yAnti.append(self.y_team.robot(i).y)
+                #xAnti = (self.y_team.robot(0).x, self.y_team.robot(1).x)
+                #yAnti = (self.y_team.robot(0).y, self.y_team.robot(1).y)
+            
+            xAnti.append(4500)
+            yAnti.append(goalUp)
+            
+            #up = []
+            #down = []
+            central = []
+            for i in range(n + 2):
+                dist = math.sqrt((x0 - xAnti[i])**2 + (y0 - yAnti[i])**2)
+                D = dist * (4500 - x0) / (xAnti[i] - x0)
+                if (R + r + 20) / dist > 1: alphaNew = math.asin(1)
+                else: alphaNew = math.asin((R + r + 20) / dist)
+                #print(4500 - x1, D, (xAnti[0] - x1) / dist)
+                gamma = math.acos((xAnti[i] - x0) / dist) - alphaNew
+                
+                downDist = math.sqrt(D**2 - (4500 - x0)**2) - (4500 - x0) * math.tan(gamma) #HASHUV
+                
+                if abs(D * math.sin(alphaNew) / downDist) <= 1: 
+                    bettaNew = math.asin(D * math.sin(alphaNew) / downDist) 
+                elif math.sin(alphaNew) > 0: bettaNew = math.asin(1)
+                else: bettaNew = math.asin(-1)
+                
+                upDist = D * math.sin(alphaNew) / math.sin(math.pi - 2 * alphaNew - bettaNew) #HASHUV
+
+                if y0 > yAnti[i]: 
+                    (downDist, upDist) = (upDist, downDist)
+                    ycc = y0 - D * math.sin(alphaNew + gamma)
+                else:
+                    ycc = y0 + D * math.sin(alphaNew + gamma)
+
+
+                #up.append(ycc + upDist)
+                #down.append(ycc - downDist)
+                if ycc < goalDown or ycc > goalUp:
+                    central.append([ycc, ycc + upDist, ycc - downDist])
+            #if yAnti[0] > yAnti[1]:
+            #    pUp = goalUp - up[0]
+            #    pMiddle = down[0] - up[1]
+            #    pDown = down[1] - goalDown
+            #else:
+            #    pUp = goalUp - up[1]
+            #    pMiddle = down[1] - up[0]
+            #    pDown = down[0] - goalDown
+            #maxi = max(pUp, pMiddle, pDown)
+            
+
+            central = sorted(central, key=lambda x: x[0])
+            #for i in range(n + 2):
+            #    print(central[i][0], end = " ")
+            #    print(central[i][0], central[i][1], central[i][2], end = " ")
+            #print()
+
+            maxiAngle = 0
+            rememberI = -1
+            for i in range(len(central) - 1):
+                lookUp = central[i + 1][2]
+                lookDown = central[i][1]
+
+                bokDown = math.sqrt((x1 - 4500)**2 + (y1 - lookDown)**2)
+                bokUp = math.sqrt((x1 - 4500)**2 + (y1 - lookUp)**2)
+                v1 = (4500 - x1, lookDown - y1)
+                v2 = (4500 - x1, lookUp - y1)
+                #if (v1[0] * v2[0] + v1[1] * v2[1]) / (bokDown * bokUp) > 1: 
+                #    angleBetweenVectors = math.acos(1)
+                #elif (v1[0] * v2[0] + v1[1] * v2[1]) / (bokDown * bokUp) < -1:
+                #     angleBetweenVectors = math.acos(-1)
+                angleBetweenVectors = math.acos((v1[0] * v2[0] + v1[1] * v2[1]) / (bokDown * bokUp))
+
+                if angleBetweenVectors > maxiAngle:
+                    maxiAngle = angleBetweenVectors
+                    rememberI = i
+                #osn = lookUp - lookDown
+                #distUp = osn * bokUp / (bokUp + bokDown)
+
+            if rememberI != -1:
+                lookUp = central[rememberI + 1][2]
+                lookDown = central[rememberI][1]
+                bokDown = math.sqrt((x1 - 4500)**2 + (y1 - lookDown)**2)
+                bokUp = math.sqrt((x1 - 4500)**2 + (y1 - lookUp)**2)
+                
+                osn = lookUp - lookDown
+                distUp = osn * bokUp / (bokUp + bokDown)
+                
+                self.xR = 4500
+                self.yR = lookUp - distUp
+            else:
+                self.xR = 4500
+                self.yR = 0
+            #print()
+            #print("UP: ", up, "DOWN: ", down, "XR: ", self.xR, "YR: ", self.yR);
+            #self.state = 1
+
+            if self.ball.x - self.xR != 0: k = (self.ball.y - self.yR) / (self.ball.x - self.xR)
+            else: k = 0
+            b = self.yR - k * self.xR
+
+            xTo = self.ball.x - (R + r + 20)
+            yTo = k * xTo + b
+
+            if x1 > x0 and abs(x1 - x0) > (R + r + 20): #D > 50:
+                d = math.sqrt((x0 - x1)**2 + (y0 - y1)**2)
+                rr = R + r + 300 #2
+                if rr / d > 1: d = rr
+                if d**2 - rr**2 >= 0: hyp = math.sqrt(d**2 - rr**2)
+                else: hyp = 0
+
+                alpha = math.asin(rr / d)
+                sigma2 = math.atan2(abs(y0 - y1), abs(x0 - x1)) - alpha
+                if y0 > y1: yTo = y1 + hyp * math.sin(sigma2)
+                else: yTo = y1 - hyp * math.sin(sigma2)
+                xTo = x1 - hyp * math.cos(sigma2)
+
+                #xTo, yTo = xTo1, yTo1
+            
+            #self.b_team.robot(0).autoKick = 0.0
+            #distanceToChangeState = abs(x1 * (yR - y0) - y1 * (xR - x0) + xR * y0 - yR * x0) / math.sqrt((yR - y0)**2 + (xR - x0)**2)
+
+            #if abs(x1 - xTo) < 130 and abs(y1 - yTo) < 80 and self.state == 0:
+            if self.state == 0:
+                distanceToChangeState = abs(x1 * (self.yR - y0) - y1 * (self.xR - x0) + self.xR * y0 - self.yR * x0) / math.sqrt((self.yR - y0)**2 + (self.xR - x0)**2)
+                #print(distanceToChangeState)
+                if distanceToChangeState < 45 and (x1 - xTo) < 130:
+                    self.state = 1
+                    self.b_team.robot(0).autoKick = 1.0
+
+            #print(self.state, self.yR, xTo, yTo, sep = "    ")
+            #if self.state == 0:
+
+            if self.state == 1:
+                #self.b_team.robot(0).autoKick = 1.0
+                if not self.wasBallKicked:
+                    self.firstBallX = x0
+                    self.wasBallKicked = True
+                
+                xTo = self.firstBallX + (R + r + 140)
+                yTo = k * xTo + b
+                if x1 - self.firstBallX > 50 or abs(self.firstBallX - x0) > 250:
+                    self.b_team.robot(0).autoKick = 0.0
+                    self.firstBallX = -6000
+                    self.wasBallKicked = False
+                    self.state = 0
+
+            #print(xTo, yTo);
+            self.b_team.robot(0).go_to_point(auxiliary.Point(xTo, yTo))
+            #print(self.state, self.dec)
+            #print(x1, y1, xTo, yTo)
+            #print("GO")
+            #self.b_team.robot(0).go_to_point(auxiliary.Point(0, 0))
 
             for i in range(const.TEAM_ROBOTS_MAX_COUNT):
                 if self.b_team.robot(i).isUsed:
--- bridge/processors/robot.py
+++ bridge/processors/robot.py
@@ -43,8 +43,15 @@ class Robot:
     def go_to_point_with_detour(self, target_point, y_team, b_team):
         pass
 
-    def go_to_point(self, point, is_final_point):
-        pass
+    def go_to_point(self, point):
+        g = math.sqrt((point.x - self.x)**2 +  (point.y - self.y)**2)
+        if g == 0: g = 1
+        b = math.acos((point.x - self.x)/g)
+        cosRazn = ((point.x - self.x) * math.cos(self.orientation) + (point.y - self.y) * math.sin(self.orientation)) / g
+        sinRazn = ((point.y - self.y) * math.cos(self.orientation) - (point.x - self.x) * math.sin(self.orientation)) / g
+        self.speedX = cosRazn * 0.025 * g #0.03
+        self.speedY = sinRazn * 0.025 * g #0.03
+        #self.orientation  
 
         if const.IS_SIMULATOR_USED:
             self.speedY *= -1
@@ -54,6 +61,16 @@ class Robot:
 
 
     def rotate_to_point(self, point):
-        pass
+        (vecX1, vecY1) = (point.x - self.x, point.y - self.y)
+        (vecX2, vecY2) = (1, 0)#(math.cos(self.orientation), math.sin(self.orientation))
+        d1 = math.sqrt(vecX1**2 + vecY1**2)
+        d2 = math.sqrt(vecX2**2 + vecY2**2)
+        angle = math.acos((vecX1*vecX2 + vecY1*vecY2) / (d1 * d2))
+        if vecY1 < 0: angle *= -1
+        
+        razn = abs(angle - self.orientation)
+        if razn < 2*math.pi - razn: self.speedR = (angle - self.orientation) * 20#16
+        else: self.speedR = (angle - self.orientation - 2*math.pi) * 20
+
         if const.IS_SIMULATOR_USED:
             pass
